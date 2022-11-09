a31e5c0170698995c6a3fa1d4a302528a5904f93
sidiqasim <sidiqasim@yahoo.com>
2022-11-09 09:43:09 -0500

    this is the updated class

--- src/gitPractice2/theUpdatedClass.java
+++ src/gitPractice2/theUpdatedClass.java
@@ -1,11 +1,12 @@
 package gitPractice2;
 
-public class Test2 {
+public class theUpdatedClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is an update to the test2 file");
+		
+		System.out.println("This is the updated class");
 
 	}
 
